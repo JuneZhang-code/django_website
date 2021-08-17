9d3275f034a538482b960de75104a8116144737a
JuneZhang-code <June1208@4b5d82362e5e>
2021-08-17 01:50:45 +0000

    Remove old stuff

--- OR1GIN_STUDIO/settings.py
+++ OR1GIN_STUDIO/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w%h-ade-y#$+#nyv&c%6yl@co#(q4qiaeczw@i3btmtqrpw@c^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['june1208.pythonanywhere.com']
 
 
 # Application definition
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
